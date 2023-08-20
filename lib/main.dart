de0ba53c99d3a721548c670e11c7dbed0a66fac5
Ariel <ariel.calderon@epn.edu.ec>
2023-08-20 06:25:32 -0500

    Primmer commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentScreen = 0;
+  List<Widget> pages = const [LoginScreen()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentScreen],
+    );
+  }
+}
